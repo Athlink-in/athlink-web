1fe4b8f8329a68fcd5a251ef884ed26895e1509a
Ishaan Karvir <ikarvir@calpoly.edu>
2022-01-16 16:56:59 -0800

    Changed src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and my name is Athlink.
+          Edit <code>src/App.js</code> and your name is Athlink.
         </p>
         <a
           className="App-link"
