1031c493cde0d801d6b576347f390d067739cd30
Ishaan Karvir <ikarvir@calpoly.edu>
2022-01-30 17:40:48 -0800

    Linting setup

--- src/mainContent.js
+++ src/mainContent.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function MainContent(props) {
-    return (
-        <div>
-            hi there            
-        </div>
-    );
+function MainContent() {
+  return (
+    <div>
+      hi there
+    </div>
+  );
 }
 
 export default MainContent;
