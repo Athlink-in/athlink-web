df731e1a850096a2b8465c71a7509484f3582783
Brandon Burana <bburana@calpoly.edu>
2022-02-15 00:39:20 -0800

    Made edit modal and style changes

---
+++ src/EditProfileModal.js
@@ -0,0 +1,216 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from '@mui/material';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import OutlinedInput from '@mui/material/OutlinedInput';
+// import Input from '@mui/material/Input';
+// import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+// import Divider from '@mui/material/Divider';
+import axios from 'axios';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+export default function EditProfileModal(props) {
+  const [open, setOpen] = useState(false);
+  const { formValue, setFormValue } = props;
+  const [editedFormValue, setEditedFormValue] = useState(formValue);
+
+  console.log('inside edit profile');
+  console.log(editedFormValue);
+  // const [formValue, setFormValue] = useState({
+  //   firstname: '',
+  //   lastname: '',
+  //   email: 'keeratg@gmail.com',
+  //   memberSince: null,
+  //   height: 0,
+  //   age: 0,
+  //   school: '',
+  //   weight: 0,
+  //   gradYear: 2022,
+  //   sex: '',
+  // });
+
+  useEffect(() => {
+    setEditedFormValue(formValue);
+  }, [formValue]);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (e) => {
+    setEditedFormValue({
+      ...editedFormValue,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async () => {
+    // e.preventDefault();
+    const backend = `${process.env.REACT_APP_BACKEND_HOST}/user`;
+    // console.log(formValue);
+    // console.log(backend);
+    axios.post(backend, editedFormValue).then(
+      () => setFormValue(editedFormValue),
+    ).catch(
+      (error) => console.log(error),
+    );
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen} variant='contained'>
+        Edit Profile
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <Typography
+            variant="h4"
+            align='left'
+            fontWeight='bold'
+            sx={{ mt: 3 }}
+          >
+            Edit Profile
+          </Typography>
+          <hr style={{ marginBottom: 20 }} />
+          <TextField
+            required
+            fullWidth
+            onChange={handleChange}
+            id="firstname"
+            label="First name"
+            defaultValue={editedFormValue.firstname}
+            sx={{ mb: 3 }}
+          />
+
+          <TextField
+            required
+            fullWidth
+            onChange={handleChange}
+            id="lastname"
+            label="Last name"
+            defaultValue={editedFormValue.lastname}
+            sx={{ mb: 3 }}
+          />
+
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <TextField
+                required
+                fullWidth
+                onChange={handleChange}
+                id="school"
+                label="School"
+                defaultValue={editedFormValue.school}
+                sx={{ mb: 3 }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                required
+                fullWidth
+                onChange={handleChange}
+                id="gradYear"
+                label="Year"
+                defaultValue={editedFormValue.gradYear}
+                sx={{ mb: 3 }}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                required
+                fullWidth
+                onChange={handleChange}
+                id="age"
+                label="Age"
+                defaultValue={editedFormValue.age}
+                sx={{ mb: 3 }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                required
+                fullWidth
+                onChange={handleChange}
+                id="sex"
+                label="Sex"
+                defaultValue={editedFormValue.sex}
+                sx={{ mb: 3 }}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <FormControl sx={{ mb: 3 }} variant="outlined">
+                <OutlinedInput
+                  id="height"
+                  defaultValue={editedFormValue.height}
+                  onChange={handleChange}
+                  endAdornment={<InputAdornment position="end">inches</InputAdornment>}
+                />
+                <FormHelperText id="outlined-weight-text">Height</FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl sx={{ mb: 3 }} variant="outlined">
+                <OutlinedInput
+                  id="weight"
+                  defaultValue={editedFormValue.weight}
+                  onChange={handleChange}
+                  endAdornment={<InputAdornment position="end">lbs</InputAdornment>}
+                />
+                <FormHelperText id="outlined-weight-text">Weight</FormHelperText>
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          {/* <TextField
+            required
+            fullWidth
+            multiline
+            onChange={handleChange}
+            id="description"
+            label="Bio/Description"
+            defaultValue={editedFormValue.description}
+            sx={{ mb: 3 }}
+            rows={5}
+          /> */}
+
+          <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+            <Button type='submit' onClick={handleSubmit} variant='contained'>
+              Save
+            </Button>
+          </Box>
+
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/Profile.js
+++ src/Profile.js
@@ -4,39 +4,17 @@ import Typography from '@mui/material/Typography';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Paper from '@mui/material/Paper';
-import { Button } from '@mui/material';
-import Modal from '@mui/material/Modal';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
-import TextField from '@mui/material/TextField';
-// import Input from '@mui/material/Input';
-// import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-// import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-// import Divider from '@mui/material/Divider';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth } from './contexts/authContext';
 import NavBar from './NavBar';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
+import PostModal from './PostModal';
+import EditProfileModal from './EditProfileModal';
 
 function createData(formValue, rowNum) {
   const res = [];
@@ -72,167 +50,6 @@ function DenseTable(props) {
         </TableBody>
       </Table>
     </TableContainer>
-  );
-}
-
-function EditProfile(props) {
-  const [open, setOpen] = useState(false);
-  const { formValue, setFormValue } = props;
-  const [editedFormValue, setEditedFormValue] = useState(formValue);
-
-  console.log('inside edit profile');
-  console.log(editedFormValue);
-  // const [formValue, setFormValue] = useState({
-  //   firstname: '',
-  //   lastname: '',
-  //   email: 'keeratg@gmail.com',
-  //   memberSince: null,
-  //   height: 0,
-  //   age: 0,
-  //   school: '',
-  //   weight: 0,
-  //   gradYear: 2022,
-  //   sex: '',
-  // });
-
-  useEffect(() => {
-    setEditedFormValue(formValue);
-  }, [formValue]);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleChange = (e) => {
-    setEditedFormValue({
-      ...editedFormValue,
-      [e.target.id]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async () => {
-    // e.preventDefault();
-    const backend = `${process.env.REACT_APP_BACKEND_HOST}/user`;
-    // console.log(formValue);
-    // console.log(backend);
-    axios.post(backend, editedFormValue).then(
-      () => setFormValue(editedFormValue),
-    ).catch(
-      (error) => console.log(error),
-    );
-  };
-
-  return (
-    <div>
-      <Button onClick={handleOpen} sx={{ height: 20, color: 'white', backgroundColor: '#4976BA' }}>
-        Edit Profile
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-      >
-        <Box sx={{ ...style, width: 400 }}>
-          <Typography
-            variant="h4"
-            align='left'
-            fontWeight='bold'
-            sx={{ mt: 3 }}
-          >
-            Edit Profile
-          </Typography>
-          <hr style={{ marginBottom: 20 }} />
-          <TextField
-            required
-            fullWidth
-            onChange={handleChange}
-            id="firstname"
-            label="First name"
-            defaultValue={editedFormValue.firstname}
-            sx={{ mb: 3 }}
-          />
-
-          <TextField
-            required
-            fullWidth
-            onChange={handleChange}
-            id="lastname"
-            label="Last name"
-            defaultValue={editedFormValue.lastname}
-            sx={{ mb: 3 }}
-          />
-
-          <FormControl fullWidth sx={{ mb: 3 }} variant="outlined">
-            <OutlinedInput
-              id="weight"
-              defaultValue={editedFormValue.weight}
-              onChange={handleChange}
-              endAdornment={<InputAdornment position="end">lbs</InputAdornment>}
-            />
-            <FormHelperText id="outlined-weight-text">Weight</FormHelperText>
-          </FormControl>
-
-          <FormControl fullWidth sx={{ mb: 3 }} variant="outlined">
-            <OutlinedInput
-              id="height"
-              defaultValue={editedFormValue.height}
-              onChange={handleChange}
-              endAdornment={<InputAdornment position="end">inches</InputAdornment>}
-            />
-            <FormHelperText id="outlined-weight-text">Height</FormHelperText>
-          </FormControl>
-
-          <TextField
-            required
-            fullWidth
-            onChange={handleChange}
-            id="age"
-            label="Age"
-            defaultValue={editedFormValue.age}
-            sx={{ mb: 3 }}
-          />
-
-          <TextField
-            required
-            fullWidth
-            onChange={handleChange}
-            id="school"
-            label="School"
-            defaultValue={editedFormValue.school}
-            sx={{ mb: 3 }}
-          />
-
-          <TextField
-            required
-            fullWidth
-            onChange={handleChange}
-            id="gradYear"
-            label="Year"
-            defaultValue={editedFormValue.gradYear}
-            sx={{ mb: 3 }}
-          />
-
-          <TextField
-            required
-            fullWidth
-            onChange={handleChange}
-            id="sex"
-            label="Sex"
-            defaultValue={editedFormValue.sex}
-            sx={{ mb: 3 }}
-          />
-
-          <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
-            <Button type='submit' onClick={handleSubmit} sx={{ height: 50, color: 'white', backgroundColor: '#4976BA' }}>
-              Save
-            </Button>
-          </Box>
-
-        </Box>
-      </Modal>
-    </div>
   );
 }
 
@@ -277,7 +94,7 @@ export default function About() {
     <Grid container component='main'>
       <NavBar />
       {/* top section  */}
-      <Grid item xs={12} sm={6} md={6} sx={{ backgroundColor: 'white' }}>
+      <Grid item xs={12} sm={5} md={5} sx={{ backgroundColor: 'white' }}>
 
         <Box
           sx={{
@@ -305,7 +122,7 @@ export default function About() {
               src={currentUser.multiFactor.user.photoURL}
             />
             <Box sx={{ ml: 'auto', mt: 5, mr: 10 }}>
-              <EditProfile formValue={formValue} setFormValue={setFormValue} />
+              <EditProfileModal formValue={formValue} setFormValue={setFormValue} />
             </Box>
           </Box>
 
@@ -379,6 +196,8 @@ export default function About() {
           Show all videos here
         </TabPanel>
       </Grid>
+
+      <PostModal />
 
     </Grid>
   );
