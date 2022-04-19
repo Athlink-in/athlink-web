a146a39dca737b3da500a1823e081172a83c2475
Keerat Grewal <47310025+Keerat-Grewal@users.noreply.github.com>
2022-04-19 00:38:43 -0700

    Modified .env and websocket url for specific user

--- src/contexts/websocketContext.js
+++ src/contexts/websocketContext.js
@@ -1,16 +1,15 @@
-/* eslint-disable */
-
 import React, { useContext, createContext, useEffect } from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import { useAuth } from './authContext';
 
-const url = 'ws://localhost:8080/testWebsocket2/keeratg@gmail.com';
+const { currentUser } = useAuth();
+const url = `${process.env.REACT_APP_BACKEND_HOST}/testWebsocket2/${currentUser.multiFactor.user.email}`;
 
 const ws = new W3CWebSocket(url);
 
 ws.onopen = () => {
   console.log('connected to websocket!');
 };
-
 
 const WebsocketContext = createContext(ws);
 
